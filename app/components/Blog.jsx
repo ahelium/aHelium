c6a14737c32a43e8b4e44c5d40f940d926cffbc0
Anna Glander <anna.glander@gmail.com>
2015-06-27 20:33:08 -0400

    COLEMAN WROTE POSTS!

--- app/components/Blog.jsx
+++ app/components/Blog.jsx
@@ -1,11 +1,34 @@
 var React = require('react');
+var _posts = require('../posts/posts');
 
+
+var Post = React.createClass({
+
+	render: function() {
+		var title = this.props.postData.title;
+		var content = this.props.postData.content;
+
+		return (<div>
+			<h3>{title}</h3>
+			<p>{content}</p>
+		</div>);
+	}
+})
 
 var Blog = React.createClass({
 
+	getInitialState: function() {
+		console.log(_posts)
+		return {posts: _posts.posts}
+	},
+
 	render: function() {
+		console.log("my state is", this.state.posts);
 		return <div>
 			<h1>aHelium Blog</h1>
+			{this.state.posts.map(function(post) {
+					return <Post postData={post} whoa={"ffffuuuuck"} />
+				})}
 		</div>
 	}
 });
---
+++ app/posts/posts.js
@@ -0,0 +1,15 @@
+var posts = {posts:[
+	{
+		title: "My first post",
+		content: "Wow this is create but what about Anna\'s code. What about newlines? What is happening?"
+	},
+	{
+		title: "My second post",
+		content: "Wow this is create but what about Anna\'s code. What about newlines? What is happening?"
+	}
+]};
+
+module.exports = posts;
+
+
+
